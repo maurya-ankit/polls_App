457738c5c9c7d74c274a648606e8581bf3fa300f
Ankit maurya <ankitmauryavector@gmail.com>
2020-05-10 08:07:29 +0530

    initial commit

---
+++ polls/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date Published')
+
+    def was_published_recentely(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class SiteVisits(models.Model):
+    counts = models.IntegerField(default=0)
+
+    def __str__(self):
+        rval = 'total site visits : ' + str(self.counts)
+        return rval
+
+
+class createUser(models.Model):
+    username= models.CharField(max_length=50)
+    password= models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.username)
---
+++ polls/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('id')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'polls/index.html', context)
+
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not Exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# an efficient way to do it
+
+def detail(request, question_id, ):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def about(request):
+    return render(request, 'polls/about.html')
+
+
+# def signup(request):
+#     return render(request, 'polls/signup.html')
+#
+def signin(request):
+    return render(request, 'polls/signin.html')
